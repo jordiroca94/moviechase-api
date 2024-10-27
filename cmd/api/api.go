7dad9ef5d7b58189f18d3d307d47db4845269b3c
Jordi Roca <jordi.roca@acaitravel.com>
2024-10-27 16:13:10 +0100

    fix: cors errors

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -21,6 +21,23 @@ func NewAPIServer(addr string, db *sql.DB) *APIServer {
 	}
 }
 
+// Middleware function for handling CORS
+func enableCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow all origins
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		// If it's a preflight request, we end it here
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
+
 func (s *APIServer) Run() error {
 	router := mux.NewRouter()
 	subrouter := router.PathPrefix("/api/v1").Subrouter()
@@ -31,5 +48,5 @@ func (s *APIServer) Run() error {
 
 	fmt.Println("Server is running on port", s.addr)
 
-	return http.ListenAndServe(s.addr, router)
+	return http.ListenAndServe(s.addr, enableCors(router))
 }
