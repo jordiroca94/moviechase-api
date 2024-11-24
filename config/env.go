deebec5433a4c3d118b18fae59d653bd5f2c4ede
Jordi Roca <jordi.roca@acaitravel.com>
2024-11-24 19:19:36 +0100

    fix: env updated

--- config/env.go
+++ config/env.go
@@ -24,20 +24,12 @@ var Envs = initConfig()
 func initConfig() Config {
 	godotenv.Load()
 	return Config{
-		// PublicHost:             getEnv("PUBLIC_HOST", ""),
-		// Port:                   getEnv("PORT", "8080"),
-		// DBUser:                 getEnv("DB_USER", ""),
-		// DBPassword:             getEnv("DB_PASSWORD", ""),
-		// DBAdress:               fmt.Sprintf("%s:%s", getEnv("DB_HOST", "127.0.0.1"), getEnv("DB_PORT", "3306")),
-		// DBName:                 getEnv("DB_NAME", ""),
-		// JWTSecret:              getEnv("JWT_SECRET", ""),
-		// JWTExpirationInSeconds: getEnvAsInt("JWT_EXP", 3600*24*7),
 		PublicHost:             getEnv("PUBLIC_HOST", ""),
-		Port:                   getEnv("PORT", "8080"),
-		DBUser:                 getEnv("MYSQLUSER", ""),
-		DBPassword:             getEnv("MYSQL_ROOT_PASSWORD", ""),
-		DBAdress:               fmt.Sprintf("%s:%s", getEnv("MYSQL_HOST", ""), getEnv("MYSQL_PORT", "3306")),
-		DBName:                 getEnv("MYSQL_DATABASE", ""),
+		Port:                   getEnv("PORT", ""),
+		DBUser:                 getEnv("DB_USER", ""),
+		DBPassword:             getEnv("DB_PASSWORD", ""),
+		DBAdress:               fmt.Sprintf("%s:%s", getEnv("DB_HOST", ""), getEnv("DB_PORT", "")),
+		DBName:                 getEnv("DB_NAME", ""),
 		JWTSecret:              getEnv("JWT_SECRET", ""),
 		JWTExpirationInSeconds: getEnvAsInt("JWT_EXP", 3600*24*7),
 	}
