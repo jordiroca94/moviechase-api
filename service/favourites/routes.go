b219959cd69e9b425f086366d708d37306593eea
Jordi Roca <jordi.roca@acaitravel.com>
2024-12-17 00:01:23 +0100

    fix: Queries added to routes

--- service/favourites/routes.go
+++ service/favourites/routes.go
@@ -7,6 +7,6 @@ import (
 func RegisterFavouritesRoutes(router *mux.Router, handler *FavouritesHandler) {
 	router.HandleFunc("/favourites/add", handler.handleAddFavourite).Methods("POST")
 	router.HandleFunc("/favourites/delete", handler.handleDeleteFavourite).Methods("DELETE")
-	router.HandleFunc("/favourites", handler.handleGetFavourites).Methods("GET")
-	router.HandleFunc("/favourite", handler.handleGetFavourite).Methods("GET")
+	router.HandleFunc("/favourites", handler.handleGetFavourites).Queries("user_id", "{user_id}", "type", "{type}").Methods("GET")
+	router.HandleFunc("/favourite", handler.handleGetFavourite).Queries("user_id", "{user_id}", "type", "{type}", "id", "{id}").Methods("GET")
 }
